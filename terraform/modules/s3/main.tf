03a1fe7dee8424c9938eb8a91c2778d4a1457173
GonzaloRossin <gonzarossin@gmail.com>
2023-05-14 16:44:32 -0300

    add objects to the bucket and enabled versioning

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -17,6 +17,13 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 
 }
 
+resource "aws_s3_bucket_versioning" "file-versioning" {
+  bucket = aws_s3_bucket.cloud_website.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
     sid = "PublicReadGetObject"
@@ -26,7 +33,7 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
     }
 
     actions = [
-      "s3:GetObject",
+      "s3:GetObject"
     ]
 
     resources = [
@@ -39,4 +46,34 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.cloud_website.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+resource "aws_s3_object" "html" {
+  for_each = fileset("./resources/html/", "**/*.html")
+
+  bucket = aws_s3_bucket.cloud_website.id
+  key    = each.value
+  source = "./resources/html/${each.value}"
+  etag   = filemd5("./resources/html/${each.value}")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "css" {
+  for_each = fileset("./resources/css/", "**/*.css")
+
+  bucket = aws_s3_bucket.cloud_website.id
+  key    = each.value
+  source = "./resources/css/${each.value}"
+  etag   = filemd5("./resources/css/${each.value}")
+  content_type = "text/css"
+}
+
+resource "aws_s3_object" "js" {
+  for_each = fileset("./resources/js/", "**/*.js")
+
+  bucket = aws_s3_bucket.cloud_website.id
+  key    = each.value
+  source = "./resources/js/${each.value}"
+  etag   = filemd5("./resources/js/${each.value}")
+  content_type = "application/javascript"
 }
